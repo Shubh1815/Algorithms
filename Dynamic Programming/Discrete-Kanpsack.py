adc592704b22d1e0e0e86053b83e2639c3baa243
Shubh1815 <59529068+Shubh1815@users.noreply.github.com>
2020-08-11 11:42:21 +0530

    add discrete-kanpsack

---
+++ Dynamic Programming/Discrete-Kanpsack.py
@@ -0,0 +1,22 @@
+def discrete_knapsack(wp, cap):
+	dp = [[0 for i in range(cap + 1)] for j in range(len(wp) + 1)]
+	
+	for i in range(1, len(wp) + 1):
+		w, p = wp[i - 1]
+		for j in range(cap + 1):
+			index = j - w 
+			maxi = dp[i - 1][j]
+			if index >= 0:
+				maxi = max(maxi, dp[i - 1][index] + p)
+
+			dp[i][j] = maxi
+
+	return dp[len(wp)][cap]
+
+cap, n = map(int, input().split())
+
+weights = list(map(int, input().split()))
+profit = list(map(int, input().split()))
+
+wp = list(zip(weights, profit))
+print(discrete_knapsack(wp, cap))
