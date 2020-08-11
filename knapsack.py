290b87e4436347f3d34fae5dd78b2012f93913aa
Shubh1815 <59529068+Shubh1815@users.noreply.github.com>
2020-08-11 11:40:36 +0530

    fractional-knapsack

--- knapsack.py
+++ knapsack.py
@@ -1,32 +1,3 @@
-def knapsack(wp, cap):
-	dp = [0 for _ in range(cap + 1)]
-
-	for i in range(1, cap + 1):
-		maxi = 0
-		for w, p in wp:
-			index = i - w
-			if index >= 0:
-				maxi = max(maxi, dp[index] + p)
-		dp[i] = maxi
-
-	print(dp)
-	return dp[cap] 
-
-def discrete_knapsack(wp, cap):
-	dp = [[0 for i in range(cap + 1)] for j in range(len(wp) + 1)]
-	
-	for i in range(1, len(wp) + 1):
-		w, p = wp[i - 1]
-		for j in range(cap + 1):
-			index = j - w 
-			maxi = dp[i - 1][j]
-			if index >= 0:
-				maxi = max(maxi, dp[i - 1][index] + p)
-
-			dp[i][j] = maxi
-
-	return dp[len(wp)][cap]
-
 def greedy_knapsack(wp, cap):
 	for i in range(len(wp)):
 		w, p = wp[i]
@@ -50,4 +21,4 @@ weights = list(map(int, input().split()))
 profit = list(map(int, input().split()))
 
 wp = list(zip(weights, profit))
-print(discrete_knapsack(wp, cap))
+print(greedy_knapsack(wp, cap))
